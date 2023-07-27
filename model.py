9d2bd415de2e1722d0fae6f9f6c6cf084ad95b81
maneesh kumar singh <30859270+maneeshkumarsingh@users.noreply.github.com>
2023-07-27 23:34:46 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,6 +2,28 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+# Functions and procedures
+def plot_predictions(train_data, train_labels,  test_data, test_labels,  predictions):
+  """
+  Plots training data, test data and compares predictions.
+  """
+  plt.figure(figsize=(6, 5))
+  # Plot training data in blue
+  plt.scatter(train_data, train_labels, c="b", label="Training data")
+  # Plot test data in green
+  plt.scatter(test_data, test_labels, c="g", label="Testing data")
+  # Plot the predictions in red (predictions were made on the test data)
+  plt.scatter(test_data, predictions, c="r", label="Predictions")
+  # Show the legend
+  plt.legend(shadow='True')
+  # Set grids
+  plt.grid(which='major', c='#cccccc', linestyle='--', alpha=0.5)
+  # Some text
+  plt.title('Model Results', family='Arial', fontsize=14)
+  plt.xlabel('X axis values', family='Arial', fontsize=11)
+  plt.ylabel('Y axis values', family='Arial', fontsize=11)
+  # Show
+  plt.savefig('model_results.png', dpi=120)
 
 
 
@@ -68,7 +90,7 @@ model.fit(X_train, y_train, epochs=100)
 
 # Make and plot predictions for model_1
 y_preds = model.predict(X_test)
-##plot_predictions(train_data=X_train, train_labels=y_train,  test_data=X_test, test_labels=y_test,  predictions=y_preds)
+plot_predictions(train_data=X_train, train_labels=y_train,  test_data=X_test, test_labels=y_test,  predictions=y_preds)
 
 
 # Calculate model_1 metrics
